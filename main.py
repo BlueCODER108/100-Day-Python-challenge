2831d31c05098997836c03a79cf9ca597f9fe667
IdealBits <118447091+BlueCODER108@users.noreply.github.com>
2022-12-04 07:54:45 +0000

    added another comment

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ I will make sure to spend some time every day coding along, for a minimum of 10 
 I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from middle of a field!
 I am feeling 😘🤓
 You can follow my progress at replit.com/@Bluecoder108""")
+print("trying my best to change")
